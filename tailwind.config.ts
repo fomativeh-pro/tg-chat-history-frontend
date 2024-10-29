55fc631d265186f0cfa1bd762c7127fb003bd2a7
fomativeh <ufomaitive@gmail.com>
2024-10-29 02:57:30 +0100

    .

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        'spin-slow': 'spin 5s linear infinite',
+        'spin-fast': 'spin 0.5s linear infinite',
       },
     },
   },
